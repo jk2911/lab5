5cb5c5d1d0143a12e6528aee0856c0b2d7730ba2
Maxim Goy <maksgoy2911@gmail.com>
2022-10-26 17:24:07 +0300

    add menu

--- app/src/main/java/maxim/goy/lab5/MainActivity.java
+++ app/src/main/java/maxim/goy/lab5/MainActivity.java
@@ -3,6 +3,10 @@ package maxim.goy.lab5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add:
+                toast();
+                return true;
+            default:
+                return true;
+        }
+    }
+
+    public void toast() {
+        Toast.makeText(this, "Adeddddddddd", Toast.LENGTH_SHORT).show();
     }
 }
